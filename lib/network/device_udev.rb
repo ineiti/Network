5ca474874166ea1c87b5ce08b36518b33604faec
Linus Gasser <ineiti@linusetviviane.ch>
2014-11-17 20:25:38 +0100

    Forgot things with deploy

--- lib/network/device_udev.rb
+++ lib/network/device_udev.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/env ruby
 require 'drb/drb'
 
-exit unless dev = DRbObject.new_with_uri 'druby://localhost:9000'
+exit unless dev = DRbObject.new_with_uri('druby://localhost:9000')
 
-Kernel.system("echo #{ARGV.inspect} >> /tmp/dudev")
+Kernel.system("echo device_udev: #{ARGV.inspect} >> /tmp/dudev")
 
 case ARGV.first
   when /add/
+    Kernel.system("echo adding >> /tmp/dudev")
     dev.add_udev(*ARGV[1..2])
   when /del/
+    Kernel.system("echo deleting >> /tmp/dudev")
     dev.del_udev(*ARGV[1..2])
 end
