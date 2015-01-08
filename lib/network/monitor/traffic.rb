c17d2ae8c288720075bd372487d85bcad53cca74
Linus Gasser <ineiti@linusetviviane.ch>
2015-01-08 05:56:22 +0100

    Forgot things with deploy

--- lib/network/monitor/traffic.rb
+++ lib/network/monitor/traffic.rb
@@ -1,31 +1,31 @@
 #!/usr/bin/env ruby
 require 'helperclasses'
+require 'fileutils'
+require 'tmpdir'
 
 module Network
   module Monitor
     module Traffic
+      attr_accessor :config, :vlans, :hosts, :bw, :db, :table
       extend self
       include HelperClasses
+      extend DPuts
 
-      class Array
-        def to_i
-          collect { |v| v.to_i }
+      def setup_config(conf = {}, arg = nil)
+        case conf.class.name
+          when /String/
+            @config = ReadConfig.ruby(arg || ReadConfig.file_name(file))
+          when /Hash/
+            @config = conf.dup
+          else
+            Raise 'Config not found'
         end
-      end
-
-      class String
-        def splitn
-          split("\n")
-        end
-      end
-
-      def setup_config(file = 'grapher.conf.rb', arg = nil)
-        @config = ReadConfig.ruby(arg || ReadConfig.file_name(file))
 
         @vlans = @config._vlans || []
         @hosts = @config._hosts || []
-        @bw = @config._bw
-        @db = @config._db
+        @bw = @config._bw || false
+        @db = @config._db || 'traffic.rrd'
+        @config._bw_upper ||= 1_000_000
         @table = 'mangle'
         @old_values=[0] * ((@bw ? 3 : 0) + @vlans.length + @hosts.length)
       end
@@ -44,22 +44,22 @@ module Network
               "DS:host#{h}:COUNTER:20:0:#{@config._bw_upper}"
             }).join(' ')
 
-        ranges = [[1, 600], [6, 1440], [60, 1728]].each { |step, len|
+        ranges = [[1, 600], [6, 1440], [60, 1728]].collect { |step, len|
           %w( AVERAGE MIN MAX ).collect { |label|
-            "RRA : #{label} : 0.5 : #{step} : #{len} "
+            "RRA:#{label}:0.5:#{step}:#{len}"
           }.join(' ')
         }.join(' ')
 
         File.exists?(@db) and FileUtils.rm_f(@db)
-        System.run_bool("rrdtool create #{@db} --step 10 #{bw_int} #{counters} #{ranges}")
+        dp System.run_str(dp "rrdtool create #{@db} --step 10 #{bw_int} #{counters} #{ranges}")
       end
 
       def ld(*args)
-        puts args.join(' ')
+        dputs(1) { args.join(' ') }
       end
 
       def color(nbr)
-        c = @config._colors[nbr] || '0ff'
+        c = ( @config._colors and @config._colors[nbr] ) || '0ff'
         c.scan(/./).collect { |c| c+c }.join
       end
 
@@ -77,45 +77,45 @@ module Network
         end
 
         defs, cdefs, lines = '', '', ''
-        @vlans.split.each { |v|
+        @vlans.each { |v|
           defs += " DEF:vlan#{v}=#{@db}:vlan#{v}:AVERAGE"
           cdefs += " CDEF:bvlan#{v}=vlan#{v},8,*"
           col = color(v)
           lines += " AREA:bvlan#{v}##{col}:#{@config._names_vlans[v]}:STACK"
         }
-        @hosts.split.each { |h|
+        @hosts.each { |h|
           defs += " DEF:host#{h}=#{@db}:host#{h}:AVERAGE"
           cdefs += " CDEF:bhost#{h}=host#{h},8,*"
-          col = color(v)
-          lines +=" AREA:bhost#{h}##{col}:#{@config._names_hosts[h]}:STACK"
+          col = color(h)
+          lines +=" AREA:bhost#{h}##{col}:#{@config._name_hosts[h]}:STACK"
         }
 
         Dir.mktmpdir { |d|
           [['vlans-hour.png', -1800, 10],
            ['vlans-halfday.png', -43200, 240],
-           ['vlans-week.png', 86400 * 7, 3600]].each { |png, start, step|
+           ['vlans-week.png', -86400 * 7, 3600]].each { |png, start, step|
             file = "#{d}/tmp"
             args = "graph -u #{@config._bw_upper} #{file} --start #{start} --step #{step} "
-            args += "-a PNG -t 'Traffic to internet' --vertical-label 'bps' -w 800 -h 200 -r"
+            args += "-a PNG -t 'Traffic to internet' --vertical-label 'bps' -w 800 -h 200 -r "
             args += [bw_defs, defs, bw_cdefs, cdefs].join(' ')
-            args += "LINE1 : 0 #{lines} #{bw_lines}"
-            System.run_bool("rrdtool #{args}")
+            args += " LINE1:0 #{lines} #{bw_lines}"
+            dp System.run_str(dp "rrdtool #{args}")
             File.chmod(0444, file)
-            File.mv file, png
+            FileUtils.mv file, png
           }
         }
       end
 
       def measure
         #values = %x[ iptables -t #{table} -L POST_COUNT -nvx | grep -v 172.16.0.1 ].splitn
-        values = %x[ iptables -t #{table} -L POST_COUNT -nvx ].splitn
+        values = System.run_str("iptables -t #{table} -L POST_COUNT -nvx").split("\n")
         ld values.join("\n")
         data = []
         if @bw
-          bws = %x[ tail -n 2 /var/log/check-bandwidth.log | sed -e 's/.* //' ].splitn.to_i
+          bws = System.run_str("tail -n 2 /var/log/check-bandwidth.log | sed -e 's/.* //'").split("\n").to_i
           ld bws.inspect
           data.push(bws.min, bws.max)
-          internet = %x[ grep #{@config._internet_dev} /proc/net/dev | sed -e "s/.*://" ].split
+          internet = System.run_str("grep #{@config._internet_dev} /proc/net/dev | sed -e 's/.*://' ]").split
           ld internet.inspect
           data.push internet[0].to_i
         end
@@ -127,8 +127,14 @@ module Network
           data.push bytes[1]
         }
         @hosts.each { |h|
-          bytes = values.select { |val| val =~ / #{@config._host_ips[h]} / }.map { |val|
-            val.split[1] }.to_i
+          dp h
+          dp @config
+          bytes = values.select { |val|
+            dp val
+            val =~ / #{@config._host_ips[h]} /
+          }.map { |val|
+            dp val
+            val.split[1] }.collect{|a| a.to_i }
           ld h, bytes.inspect
           data.push bytes[1]
         }
@@ -139,18 +145,19 @@ module Network
         ld @vlans.join(':')
         ld @hosts.join(':')
         labels = @bw ? 'bw_min:bw_max:total:' : ''
-        labels += (@vlans + @hosts).join(':')
-        system("rrdtool update #{@db} -t #{labels} N:#{data.join(':')}")
+        dp labels += (@vlans.collect{|v| "vlan#{v}"} +
+               @hosts.collect{|h| "host#{h}"}).join(':')
+        dp System.run_str(dp "rrdtool update #{@db} -t #{labels} N:#{data.join(':')}")
         graph_traffic
         total = 0
-        puts (vals = @old_values.zip(data)[3..-1].collect { |a, b| (b - a) * 8 / 10 }).join(':')
-        puts vals.inject(:+)
+        start = @bw ? 3 : 0
+        ld (vals = @old_values.zip(data)[start..-1].collect { |a, b| (b - a) * 8 / 10 }).join(':')
+        ld vals.inject(:+)
         @old_values = data
-        sleep 10
       end
 
       def ipt(*args)
-        System.run_bool("iptables -t #{@table} #{args.join(' ')}")
+        System.run_str(dp "iptables -t #{@table} #{args.join(' ')}")
       end
 
       def create_iptables
@@ -158,10 +165,11 @@ module Network
 
         [%w( PRE -i -d ),
          %w( POST -o -s )].each { |prefix, dir, target|
+          target_other = (target == '-d') ? '-s' : '-d'
           mangle="#{prefix}ROUTING"
           count="#{prefix}_COUNT"
           # Cleaning up
-          if ipt('-L', 'mangle') =~ /count/
+          if ipt('-L') =~ /#{count}/
             ld 'Cleaning up'
             ipt '-D', mangle, '-j', count
             ipt '-F', count
@@ -173,11 +181,13 @@ module Network
           ipt '-I', mangle, '-j', count
 
           @vlans.each { |v|
-            ipt '-A', count, dir, v, target, '172.16.0.1 -j RETURN'
+            ipt '-A', count, dir, v, target, '192.168.0.0/16 -j RETURN'
             ipt '-A', count, dir, v, '-j RETURN'
           }
+          ipt '-A', count, '-s 192.168.0.0/16 -d 192.168.0.0/16 -j RETURN'
           @hosts.each { |h|
-            ipt '-A', count, target, "#{config._hosts_ips[h]} -j RETURN"
+            ipt '-A', count, target, "#{config._host_ips[h.to_sym]} -j RETURN"
+            ipt '-A', count, target_other, "#{config._host_ips[h.to_sym]} -j RETURN"
           }
         }
       end
--- test/test_monitor_traffic.rb
+++ test/test_monitor_traffic.rb
@@ -3,13 +3,33 @@ require 'network/monitor/traffic'
 include Network::Monitor
 
 def main
-  Traffic.setup_config
-  Traffic.hosts = %w( dp mbp )
-  Traffic.config._hosts_ips = %w( 192.168.1.3 192.168.1.146 )
+  test_measure
+  #test_create_rrb
+  #test_create_iptables
+end
+
+def test_setup
+  Traffic.setup_config({hosts: %i(dp mbp),
+                        host_ips: {dp: '192.168.1.3', mbp: '192.168.1.146'},
+                        colors: {dp: 'f99', mbp: '9f9'},
+                        name_hosts: {dp: 'DreamPlug', mbp: 'MacBook_Pro'},
+                        bw_upper: 1_000_000} )
 end
 
 def test_create_rrb
+  test_setup
   Traffic.create_rrb
+end
+
+def test_create_iptables
+  test_create_rrb
+  Traffic.create_iptables
+end
+
+def test_measure
+  #test_create_iptables
+  test_setup
+  Traffic.measure
 end
 
 main
